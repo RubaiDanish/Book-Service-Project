b6e55f9829341d3bd5acf94438c6ede042e43e2e
Rubai Danish <RubaiDanish@Rubais-MBP.attlocal.net>
2021-04-15 09:48:54 -0500

    project commit

---
+++ book-service-eureka-server/src/test/java/com/trilogyed/bookserviceeurekaserver/BookServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.bookserviceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
