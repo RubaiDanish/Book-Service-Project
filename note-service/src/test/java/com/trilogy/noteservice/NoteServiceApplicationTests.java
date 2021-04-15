e8bd94995c800bfa279ae711dec3e0ac69c1d0e0
MichaelBrown1998 <78372439+MichaelBrown1998@users.noreply.github.com>
2021-04-15 11:43:50 -0500

    Add files via upload

---
+++ note-service/src/test/java/com/trilogy/noteservice/NoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogy.noteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
