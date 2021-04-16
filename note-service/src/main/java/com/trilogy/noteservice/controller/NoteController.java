7971ec0610cf7ab27033e352e655a63b9e090fb6
Adlin Ahmed <adlinahmed@MacBook-Pro.local>
2021-04-16 11:20:18 -0500

    note controller

--- note-service/src/main/java/com/trilogy/noteservice/controller/NoteController.java
+++ note-service/src/main/java/com/trilogy/noteservice/controller/NoteController.java
@@ -1,9 +1,58 @@
 package com.trilogy.noteservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.trilogy.noteservice.dao.NoteDao;
+import com.trilogy.noteservice.model.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class NoteController {
 
+    @Autowired
+    NoteDao noteDao;
 
+    @RequestMapping(value = "/notes", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Note createNote(@RequestBody Note note) {
+        return noteDao.addNote(note);
+    }
+
+    @RequestMapping (value = "/notes/{note_id}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Note getNote(@PathVariable int id) {
+        if (id < 1) {
+            throw new IllegalArgumentException("ID must be greater than 0.");
+        }
+        Note returnVal = noteDao.getNote(id);
+        if (returnVal == null) {
+            throw new IllegalArgumentException("No note with id " + id);
+        }
+
+        return returnVal;
+    }
+
+    // TODO: getNotesByBook
+
+    @RequestMapping (value = "notes", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Note> getAllNotes() {
+        return noteDao.getAllNotes();
+    }
+
+    @RequestMapping (value = "/notes/{note_id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateNote(@RequestBody Note note, @PathVariable int note_id) {
+        if (note_id != note.getNoteId()) {
+            throw new IllegalArgumentException("Book ID on path must match the ID in the Book object.");
+        }
+    }
+
+    @RequestMapping (value = "/notes/{note_id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteNote(@PathVariable int note_id) {
+
+    }
 }
